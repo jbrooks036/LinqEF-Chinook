cb75f2e113a646cfa97e9db5a7f35ed8582cd137
Jo Ann Brooks <joannbrooks036@gmail.com>
2016-06-02 16:13:36 -0500

    initial commit

---
+++ LinqEF-Chinook/Models/Artist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqEF_Chinook.Models
+{
+    public class Artist
+    {
+        public int ArtistId
+        {
+            get; set;
+        }
+        public string Name
+        {
+            get; set;
+        }
+    }
+}
---
+++ LinqEF-Chinook/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using LinqEF_Chinook.Models;
+using System.Data.Entity;
+
+namespace LinqEF_Chinook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database.SetInitializer<ChinookContext>(null);
+
+            ChinookContext dbContext = new ChinookContext();
+            var artistSearch = "Sabbath";
+            var artists = dbContext.Artist.Where(a => a.Name.Contains(artistSearch));
+
+        }
+    }
+}
